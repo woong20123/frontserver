ac8a904a04c5f46bb6695ce410467c34a30be4eb
laptop_ahn <jinny20123@gmail.com>
2020-03-30 02:12:07 +0900

    make go project #1
    vs code file

--- src/github.com/frontserver/frontserver/frontserver.go
+++ src/github.com/frontserver/frontserver/frontserver.go
@@ -4,13 +4,9 @@ package main
 import (
 	"fmt"
 	"math"
-
-	// 새로 정의한 testlib/mymath 라이브러리를 추가해서 수행합니다.
-	"testlib/mymath"
 )
 
 func main() {
 	fmt.Printf("Now you have %g problems.\n", math.Round(math.Sqrt(7)))
-	fmt.Printf("mymath Plus(%d)\n", mymath.Plus(10, 10))
-	fmt.Printf("mymath Multiple(%d)\n", mymath.Multiple(10, 10))
+
 }
