d5a6a0fd4db95071ed4b009badcdb04a020e2606
jinny20123 <jinny20123@gmail.com>
2020-04-10 18:18:46 +0900

    [make example tcp server #2]  - TCP 서버 예제 파일 추가 [To do]  - TCP 패킷의 구조를 정의하고 데이터가 작게 올때  연속적으로 받아서 패킷으로 합치는 작업 필요

--- src/github.com/frontserver/frontserver/frontserver.go
+++ src/github.com/frontserver/frontserver/frontserver.go
@@ -1,12 +1,53 @@
 package main
 
 // "factored" import statment
+
 import (
-	"fmt"
-	"math"
+	"io"
+	"log"
+	"net"
 )
 
-func main() {
-	fmt.Printf("Now you have %g problems.\n", math.Round(math.Sqrt(7)))
+const MAX_BUFFERSIZE = 4096
 
+func ConnHandler(conn net.Conn) {
+	recvBuf := make([]byte, MAX_BUFFERSIZE)
+
+	for {
+		n, err := conn.Read(recvBuf)
+		if nil != err {
+			if io.EOF == err {
+				log.Printf("connection is closed from client; %v", conn.RemoteAddr().String())
+				return
+			}
+			log.Printf("fail to receive data; err: %v", err)
+			return
+		}
+
+		if 0 < n {
+			// data parser
+			data := recvBuf[:n]
+			log.Println(string(data))
+		}
+	}
+}
+
+func main() {
+	server_listen, err := net.Listen("tcp", ":20224")
+
+	if nil != err {
+		log.Fatalf("fail to bind address to 20224; err : %v", err)
+	}
+
+	defer server_listen.Close()
+
+	for {
+		conn, err := server_listen.Accept()
+		if nil != err {
+			log.Printf("fail to accept; err: %v", err)
+			continue
+		}
+
+		go ConnHandler(conn)
+	}
 }
